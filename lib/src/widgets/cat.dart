15e1805bfe69eab601fddfd9dcf4118917125e95
danilolimadev <danilolimadev@gmail.com>
2020-03-26 20:39:53 -0300

    First Commit

---
+++ lib/src/widgets/cat.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Cat extends StatelessWidget {
+  build(context) {
+    return Image.network(
+      'https://img1.gratispng.com/20180314/yye/kisspng-black-cat-kitten-witchcraft-frightened-cat-5aa9255d82f358.6489915915210345895364.jpg',
+    );
+  }
+}
